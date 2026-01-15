9f8aa6328d50fbf9ef6946775821a30e64a59853
Ethan Guerin <129787373+EthanAGi@users.noreply.github.com>
2026-01-14 16:11:11 -0800

    Add: Vuetify and Game page
    Initialization of app, added vuetify and created game page. Vue replaced old gitignore and readme but I hope that's okay!

---
+++ assignment1/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <v-btn to="/game" variant="flat">
+  Go to Game
+</v-btn>
+
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
---
+++ assignment1/src/views/game.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container class="py-8">
+    <v-card class="mx-auto" max-width="600">
+      <v-card-title class="text-h5">The Clever Quest</v-card-title>
+      <v-card-text>
+        If life were a video game, love would be the ultimate quest. 
+      </v-card-text>
+      <v-card-actions>
+        <v-btn to="/" variant="flat">Back to Home</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
+</template>
